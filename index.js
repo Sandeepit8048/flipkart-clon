72e5b4fca66567c8b202a2433fb1d1c8ede7ed28
Sandeep Yadav <rkysandeepyadav@gmail.com>
2023-08-13 21:45:13 +0530

    landing page1

---
+++ index.js
@@ -0,0 +1,29 @@
+let slides = document.querySelectorAll(".slide")
+let about=document.getElementById("about");
+let counter = 0;
+console.log(slides);
+slides.forEach(function (slide, index)
+ { 
+    slide.style.left = `${index * 100}%`
+ }
+)
+
+function slideImage(counter) {
+    slides.forEach(function (slide)
+     { 
+        slide.style.transform = `translateX(${counter * 100+100}%)`
+     }
+    )
+}
+function gonext() {
+    // console.log(slides);
+    counter++;
+    // alert();
+    slideImage(counter)
+}
+function goprev() {
+    counter--;
+    // alert();
+    slideImage(counter)
+}
+
