75b3fcc6521785ca2455d2f6b7a3173c4ba8d26a
Sandeep Yadav <rkysandeepyadav@gmail.com>
2023-08-25 17:55:55 +0530

    Add new carousel

--- index.js
+++ index.js
@@ -127,3 +127,67 @@ function mediaManagement() {
     oldViewportWidth1 = newViewportWidth;
   }
 }
+
+
+// landing pages2
+
+const prev2 = document.querySelector("#prev2");
+const next2 = document.querySelector("#next2");
+
+let carouselVp2 = document.querySelector("#carousel-vp2");
+
+let cCarouselInner2 = document.querySelector("#cCarousel-inner2");
+let carouselInnerWidth2 = cCarouselInner.getBoundingClientRect().width;
+
+let leftValue2 = 0;
+
+// Variable used to set the carousel movement value (card's width + gap)
+const totalMovementSize2 =
+  parseFloat(
+    document.querySelector(".cCarousel-item2").getBoundingClientRect().width,
+    10
+  ) +
+  parseFloat(
+    window.getComputedStyle(cCarouselInner2).getPropertyValue("gap"),
+    10
+  );
+
+prev1.addEventListener("click", () => {
+  if (!leftValue == 0) {
+    leftValue -= -totalMovementSize;
+    cCarouselInner2.style.left = leftValue + "px";
+  }
+});
+
+next1.addEventListener("click", () => {
+  const carouselVpWidth2 = carouselVp2.getBoundingClientRect().width;
+  if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth2) {
+    leftValue -= totalMovementSize;
+    cCarouselInner2.style.left = leftValue + "px";
+  }
+});
+
+const mediaQuery5102 = window.matchMedia("(max-width: 510px)");
+const mediaQuery7702 = window.matchMedia("(max-width: 770px)");
+
+mediaQuery5102.addEventListener("change", mediaManagement);
+mediaQuery7702.addEventListener("change", mediaManagement);
+
+let oldViewportWidth2 = window.innerWidth;
+
+function mediaManagement() {
+  const newViewportWidth = window.innerWidth;
+
+  if (leftValue <= -totalMovementSize && oldViewportWidth2 < newViewportWidth) {
+    leftValue += totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+    oldViewportWidth2 = newViewportWidth;
+  } else if (
+    leftValue <= -totalMovementSize &&
+    oldViewportWidth2 > newViewportWidth
+  ) {
+    leftValue -= totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+    oldViewportWidth2 = newViewportWidth;
+  }
+}
